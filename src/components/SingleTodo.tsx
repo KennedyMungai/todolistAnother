3048098cf9863ee015011327ef2a9b0c68a237b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 21:31:38 +0300

    Added proper formatting to the draggable component

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -47,47 +47,51 @@ const SingleTodo: React.FC<Props> = ({todo, todos, setTodos, index}) => {
 
     return (
         <Draggable draggableId={todo.id.toString()} index={index}>
-            <form
-                action=""
-                className="todos__single"
-                onSubmit={(e) => handleEdit(e, todo.id)}>
+            {
+                () => (
+                    <form
+                        action=""
+                        className="todos__single"
+                        onSubmit={(e) => handleEdit(e, todo.id)}>
 
-                {
-                    edit ? (
-                        <input
-                            ref={inputRef}
-                            value={editTodo}
-                            onChange={(e) => setEditTodo(e.target.value)}
-                            className='todos__single--text' />
-                    ) : (
-                        todo.isDone ? (
-                            <s className="todos__single--text">
-                                {todo.todo}
-                            </s>
-                        ) : (
-                            <span className="todos__single--text">
-                                {todo.todo}
-                            </span>
-                        )
-                    )
-                }
-
-                <div className="">
-                    <span className="icon" onClick={() => {
-                        if (!edit && !todo.isDone) {
-                            setEdit(!edit);
+                        {
+                            edit ? (
+                                <input
+                                    ref={inputRef}
+                                    value={editTodo}
+                                    onChange={(e) => setEditTodo(e.target.value)}
+                                    className='todos__single--text' />
+                            ) : (
+                                todo.isDone ? (
+                                    <s className="todos__single--text">
+                                        {todo.todo}
+                                    </s>
+                                ) : (
+                                    <span className="todos__single--text">
+                                        {todo.todo}
+                                    </span>
+                                )
+                            )
                         }
-                    }}>
-                        <AiFillEdit />
-                    </span>
-                    <span className="icon" onClick={() => handleDelete(todo.id)}>
-                        <AiFillDelete />
-                    </span>
-                    <span className="icon" onClick={() => handleDone(todo.id)}>
-                        <MdDone />
-                    </span>
-                </div>
-            </form>
+
+                        <div className="">
+                            <span className="icon" onClick={() => {
+                                if (!edit && !todo.isDone) {
+                                    setEdit(!edit);
+                                }
+                            }}>
+                                <AiFillEdit />
+                            </span>
+                            <span className="icon" onClick={() => handleDelete(todo.id)}>
+                                <AiFillDelete />
+                            </span>
+                            <span className="icon" onClick={() => handleDone(todo.id)}>
+                                <MdDone />
+                            </span>
+                        </div>
+                    </form>
+                )
+            }
         </Draggable>
         
     )
