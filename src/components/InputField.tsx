e0cf7b7f33dd5f43513d923e664c103d6b01284f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 12:54:58 +0300

    Wired up the input field to update the todo state

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -9,7 +9,14 @@ interface Props {
 const InputField: React.FC<Props> = ({todo, setTodo}) => {
     return (
         <form className="input">
-            <input type="input" placeholder='Enter a task' className='input__box'/>
+            <input 
+                type="input" 
+                placeholder='Enter a task' 
+                className='input__box'
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                />
+
             <button className="input__submit" type='submit'>Go</button>
         </form>
     )
