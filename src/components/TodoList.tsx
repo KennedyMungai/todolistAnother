85dfa60e9b7d5df8e5483c27773d85bbc640c91f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 14:45:03 +0300

    Defined the types of todos and setTodos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Todo } from '../model';
 import './styles.css'
 
-const TodoList: React.FC = ({todos, setTodos}) => {
+interface Props {
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
     return (
         <div className='todos'>
             
