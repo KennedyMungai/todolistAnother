cb4d12ae69fc8e9c65875677089346dee33e240b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 14:28:33 +0300

    Imported the TodoList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App: React.FC = () => {
