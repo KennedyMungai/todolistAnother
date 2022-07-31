74adeba9f94e0b42c71f911b896965dbe33ffe7e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 15:04:59 +0300

    Added the parameters to the react component and made then generic

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Todo } from '../model';
 
-const SingleTodo = ({todo, todos, setTodos}) => {
+interface Props {
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
     return (
         <div>SingleTodo</div>
     )
