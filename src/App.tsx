dec6742cf3ad24eedf612be30b8e7d2e2560051a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 21:56:15 +0300

    Added logic to the onDragEnd function

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,44 @@ const App: React.FC = () => {
   };
 
   const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
 
+    if(!destination)
+    {
+      return;
+    }
+
+    if(destination.droppableId === source.droppableId && destination.index === source.index)
+    {
+      return;
+    }
+
+    let add, 
+      active = todos, 
+      complete = completedTodos;
+
+    if(source.droppableId === 'TodosList')
+    {
+      add = active[source.index];
+      active.splice(source.index);
+    }
+    else
+    {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    if(source.droppableId === 'TodosList')
+    {
+      active.splice(destination.index, 0, add);
+    }
+    else
+    {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setcompletedTodos(complete);
+    setTodos(active);
   }
 
   return (
