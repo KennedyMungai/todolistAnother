c4a67ca18b72ada8484077b49c08f8a517e4b7e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 12:27:09 +0300

    Added a button inside the form tag

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,6 +4,7 @@ const InputField = () => {
     return (
         <form className="input">
             <input type="input" placeholder='Enter a task' className='input__box'/>
+            <button className="input__submit" type='submit'>Go</button>
         </form>
     )
 }
