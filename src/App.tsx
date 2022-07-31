4b80064ce528207945872d5c0ae0d8a3aa238f0d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-31 21:05:27 +0300

    Added the onDragEnd parameter

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={() => {}}>
     <div className="App">
       <span className="heading">Todo List</span>
 
